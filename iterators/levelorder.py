08afb1f7671bbb4554967821811de6bf4f40c7e8
TrinayMitra <trinay.mitra@vaastav.tech>
2026-08-10 13:21:54 +0530

    Add type annotations to level-order tree iterator
    Improve type safety and support static type checking for level-order traversal.
    Add generic typing to LevelOrderIterator, including the root node, traversal queue, and iterator return type.

--- iterators/levelorder.py
+++ iterators/levelorder.py
@@ -1,22 +1,24 @@
 from collections import deque
+from typing import TypeVar
 
 from nodes import BinaryNode
 from .base_iterator import BinaryTreeIterator
 
+T = TypeVar("T")
 
-class LevelOrderIterator(BinaryTreeIterator):
+
+class LevelOrderIterator(BinaryTreeIterator[BinaryNode[T]]):
     """
     Traverses a binary tree level by level.
     """
 
-    def __init__(self, root: BinaryNode):
-        self._queue = deque()
+    def __init__(self, root: BinaryNode[T] | None) -> None:
+        self._queue: deque[BinaryNode[T]] = deque()
 
         if root is not None:
             self._queue.append(root)
 
-    def __next__(self):
-
+    def __next__(self) -> BinaryNode[T]:
         if not self._queue:
             raise StopIteration
 
