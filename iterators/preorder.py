b7fb435c32f5bbc34237978680acfbe0c2a13b23
TrinayMitra <trinay.mitra@vaastav.tech>
2026-08-10 13:24:35 +0530

    Add type annotations to preorder tree iterator
    Improve type safety and support static type checking for preorder traversal.
    Add generic typing to PreOrderIterator, including the root node, traversal stack, and iterator return type.

--- iterators/preorder.py
+++ iterators/preorder.py
@@ -1,21 +1,24 @@
+from typing import TypeVar
+
 from nodes import BinaryNode
 from .base_iterator import BinaryTreeIterator
 
+T = TypeVar("T")
 
-class PreOrderIterator(BinaryTreeIterator):
+
+class PreOrderIterator(BinaryTreeIterator[BinaryNode[T]]):
     """
     Traverses a binary tree in Preorder.
     Root -> Left -> Right
     """
 
-    def __init__(self, root: BinaryNode):
-        self._stack = []
+    def __init__(self, root: BinaryNode[T] | None) -> None:
+        self._stack: list[BinaryNode[T]] = []
 
         if root is not None:
             self._stack.append(root)
 
-    def __next__(self):
-
+    def __next__(self) -> BinaryNode[T]:
         if not self._stack:
             raise StopIteration
 
