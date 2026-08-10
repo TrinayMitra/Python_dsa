dc7b8748545ab40601be22101f3bb724b90c6642
TrinayMitra <trinay.mitra@vaastav.tech>
2026-08-10 13:22:28 +0530

    Add type annotations to postorder tree iterator
    Improve type safety and support static type checking for postorder traversal.
    Add generic typing to PostOrderIterator, including the root node, traversal stack, visited-state tuples, and iterator return type.

--- iterators/postorder.py
+++ iterators/postorder.py
@@ -1,23 +1,25 @@
+from typing import TypeVar
+
 from nodes import BinaryNode
 from .base_iterator import BinaryTreeIterator
 
+T = TypeVar("T")
 
-class PostOrderIterator(BinaryTreeIterator):
+
+class PostOrderIterator(BinaryTreeIterator[BinaryNode[T]]):
     """
     Traverses a binary tree in Postorder.
     Left -> Right -> Root
     """
 
-    def __init__(self, root: BinaryNode):
-        self._stack = []
+    def __init__(self, root: BinaryNode[T] | None) -> None:
+        self._stack: list[tuple[BinaryNode[T], bool]] = []
 
         if root is not None:
             self._stack.append((root, False))
 
-    def __next__(self):
-
+    def __next__(self) -> BinaryNode[T]:
         while self._stack:
-
             node, visited = self._stack.pop()
 
             if visited:
