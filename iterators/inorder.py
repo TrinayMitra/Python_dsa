947b6e39113a76d414b70e1a0fff8715e55fadd7
TrinayMitra <trinay.mitra@vaastav.tech>
2026-08-10 13:20:59 +0530

    Add type annotations to inorder tree iterator
    Improve type safety and support static type checking for inorder traversal.
    Add generic typing to InOrderIterator, including the root node, traversal stack, current node, and iterator return type.

--- iterators/inorder.py
+++ iterators/inorder.py
@@ -1,19 +1,22 @@
 from nodes import BinaryNode
 from .base_iterator import BinaryTreeIterator
 
+from typing import TypeVar
 
-class InOrderIterator(BinaryTreeIterator):
+T = TypeVar("T")
+
+
+class InOrderIterator(BinaryTreeIterator[BinaryNode[T]]):
     """
     Traverses a binary tree in Inorder.
     Left -> Root -> Right
     """
 
-    def __init__(self, root: BinaryNode):
-        self._stack = []
-        self._current = root
+    def __init__(self, root: BinaryNode[T]) -> None:
+        self._stack: list[BinaryNode[T]] = []
+        self._current: BinaryNode[T] | None = root
 
-    def __next__(self):
-
+    def __next__(self) -> BinaryNode[T]:
         while self._current is not None:
             self._stack.append(self._current)
             self._current = self._current.left
